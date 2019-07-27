8005861121ed52c70140e666b766b6739cfa8fb6
Ray Tran <r8tran@gmail.com>
2019-07-26 17:04:52 -0700

    hope this compiles

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,11 +1,12 @@
 class minecraft {
   include profile::base
+  $minecraft_directory = "opt/minecraft"
 
-  file { '/opt/minecraft':
+  file { $minecraft_directory:
     ensure => 'directory',
   }
 
-  file { 'minecraft_server_jar':
+  file { '${ minecraft_directory }/minecraft_server_jar':
     ensure => file,   
     source => "https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar",
   }
